2d8bb19854e9d96837a44380f7dbd1b30f12dff7
Paul David <37319982+soullover135@users.noreply.github.com>
2019-02-06 21:25:53 -0800

    Add files via upload

---
+++ activity/home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>welcome</title>
+	</head>
+	<body>
+		<br/><br/><br/><br/>
+		<center><br/>
+		<h2><font color="magenta">
+			<?php
+			echo "Hi ";
+			echo "<b>";
+			echo $_GET['name'];
+			echo ",";
+			echo "<br>";
+			echo "<br>";
+			echo "You are ";
+			echo $_GET['age'];
+			echo " years old";
+			?> </font></h2>
+			<br/><br/><br/>
+			<font color="blue"><h1 style="font-family:chiller;font-size:300%;"><b>Have a Great Day! :) </b></h1></font>
+			</center>
+	</body>
+</html>
